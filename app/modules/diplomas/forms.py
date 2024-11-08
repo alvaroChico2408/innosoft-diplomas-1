a10bef989a32d84674b24ec8f4969e400e08054c
alvaroChico2408 <alvarochicocastellano@gmail.com>
2024-11-08 10:56:14 +0100

    [feat] excel upload validation

--- app/modules/diplomas/forms.py
+++ app/modules/diplomas/forms.py
@@ -1,7 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from wtforms.validators import DataRequired
+from flask_wtf.file import FileAllowed, FileSize
 
 class UploadExcelForm(FlaskForm):
-    hours_excel = FileField('Sube tu archivo Excel', validators=[DataRequired()])
-    submit = SubmitField('Generar Diplomas')
+    hours_excel = FileField(
+        "Excel to generate diplomas",
+        validators=[
+            DataRequired(message="You didn't introduce any file."),
+            FileAllowed(["xlsx"], "Please upload an Excel file (.xlsx) only."),
+            FileSize(
+                max_size=20 * 1024 * 1024,  # 500 MB
+                message="File size should not exceed 500 MB.",
+            )
+        ],
+    )
+    submit = SubmitField("Generate Diplomas")
