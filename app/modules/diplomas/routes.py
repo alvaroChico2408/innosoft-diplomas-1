c67fa6dbc6f61019ec0d7fadb7c20bb7dcd4dd45
alvaroChico2408 <alvarochicocastellano@gmail.com>
2024-11-08 09:59:28 +0100

    [feat] diploma generation test button introduction

--- app/modules/diplomas/routes.py
+++ app/modules/diplomas/routes.py
@@ -1,7 +1,14 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
+from flask_login import login_required
 from app.modules.diplomas import diplomas_bp
-
 
 @diplomas_bp.route('/diplomas', methods=['GET'])
 def index():
     return render_template('diplomas/index.html')
+
+@diplomas_bp.route('/diplomas/generate', methods=['POST'])
+@login_required
+def generate():
+    print("¡Botón de generar diploma pulsado!")
+    print("Todo perfecto")
+    return redirect(url_for('diplomas.index'))
