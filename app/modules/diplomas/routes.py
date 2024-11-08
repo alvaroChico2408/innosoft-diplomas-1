baba911b05ea75a12fe572b4e107ce54a42bfad1
alvaroChico2408 <alvarochicocastellano@gmail.com>
2024-11-08 13:59:29 +0100

    [feat] added view and delete buttons

--- app/modules/diplomas/routes.py
+++ app/modules/diplomas/routes.py
@@ -1,8 +1,13 @@
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_required
+from app.modules.diplomas.models import Diploma
 from app.modules.diplomas import diplomas_bp
 from app.modules.diplomas.forms import UploadExcelForm
 from app.modules.diplomas.services import DiplomasService
+import os
+from flask import send_file
+from flask import current_app
+from app import db
 
 diplomas_service = DiplomasService()
 
@@ -28,8 +33,41 @@ def index():
 @diplomas_bp.route('/diplomas-visualization', methods=['GET'])
 @login_required
 def diplomas_visualization():
-    # Aquí pasaremos los datos de los diplomas en el futuro
-    diplomas = []  # Por ahora, una lista vacía
+    diplomas = Diploma.query.all()
     return render_template('diplomas/diplomas-visualization.html', diplomas=diplomas)
+
+
+@diplomas_bp.route('/view_diploma/<int:diploma_id>', methods=['GET'])
+@login_required
+def view_diploma(diploma_id):
+    diploma = Diploma.query.get(diploma_id)
+    if diploma and diploma.file_path:
+        file_path = os.path.join(current_app.root_path, "..", "diplomas", os.path.basename(diploma.file_path))
+        if os.path.exists(file_path):
+            return send_file(file_path)
+        else:
+            flash("The diploma file could not be found.", "error")
+    else:
+        flash("The diploma could not be found.", "error")
+    return redirect(url_for("diplomas.diplomas_visualization"))
+
+@diplomas_bp.route('/delete_diploma/<int:diploma_id>', methods=['POST'])
+@login_required
+def delete_diploma(diploma_id):
+    if request.form.get('_method') == 'DELETE':
+        try:
+            diploma = Diploma.query.get(diploma_id)
+            if diploma:
+                file_path = os.path.join(current_app.root_path, "..", "diplomas", os.path.basename(diploma.file_path))
+                if os.path.exists(file_path):
+                    os.remove(file_path)
+                db.session.delete(diploma)
+                db.session.commit()
+                flash("Diploma deleted successfully.", "success")
+            else:
+                flash("Diploma not found.", "error")
+        except Exception as e:
+            flash(f"Error deleting diploma: {e}", "error")
+    return redirect(url_for("diplomas.diplomas_visualization"))
 
 
