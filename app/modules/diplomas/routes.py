7441bcec8c34247d1432585fd783f7e08d586e18
alvaroChico2408 <alvarochicocastellano@gmail.com>
2024-11-08 10:21:36 +0100

    [feat] adaptation of routes.py to handle the file upload form

--- app/modules/diplomas/routes.py
+++ app/modules/diplomas/routes.py
@@ -1,14 +1,26 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_required
 from app.modules.diplomas import diplomas_bp
+from app.modules.diplomas.forms import UploadExcelForm
+from app.modules.diplomas.services import DiplomasService
 
-@diplomas_bp.route('/diplomas', methods=['GET'])
-def index():
-    return render_template('diplomas/index.html')
+diplomas_service = DiplomasService()
 
-@diplomas_bp.route('/diplomas/generate', methods=['POST'])
+@diplomas_bp.route('/diplomas', methods=['GET', 'POST'])
 @login_required
-def generate():
-    print("¡Botón de generar diploma pulsado!")
-    print("Todo perfecto")
-    return redirect(url_for('diplomas.index'))
+def index():
+    form = UploadExcelForm()
+    
+    if form.validate_on_submit():
+        file = form.hours_excel.data
+        if file:
+            try:
+                diplomas_service.validate_and_save_excel(file)
+                flash("Diplomas generados con éxito", "success")
+            except Exception as e:
+                flash(f"Error al procesar el archivo: {str(e)}", "error")
+        return redirect(url_for('diplomas.index'))
+    
+    return render_template('diplomas/index.html', form=form)
+
+
