9990ec0da41c1fbbea7fb74225800b7da93c6b82
Alejandro Aragon Sanchez <alejandro.as.2318@gmail.com>
2024-12-03 14:41:28 +0100

    [feat] login selenium test

--- app/modules/auth/tests/test_selenium.py
+++ app/modules/auth/tests/test_selenium.py
@@ -1,51 +1,21 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
-import time
 
 from core.environment.host import get_host_for_selenium_testing
-from core.selenium.common import initialize_driver, close_driver
+from core.selenium.common import initialize_driver
+import time
 
+class TestLogin():
+    def setup_method(self, method):
+        self.driver = initialize_driver()
+        self.vars = {}
 
-def test_login_and_check_element():
+    def teardown_method(self, method):
+        self.driver.quit()
 
-    driver = initialize_driver()
-
-    try:
+    def test_login(self):
+        self.driver.get(get_host_for_selenium_testing())
         host = get_host_for_selenium_testing()
-
-        # Open the login page
-        driver.get(f'{host}/login')
-
-        # Wait a little while to make sure the page has loaded completely
-        time.sleep(4)
-
-        # Find the username and password field and enter the values
-        email_field = driver.find_element(By.NAME, 'email')
-        password_field = driver.find_element(By.NAME, 'password')
-
-        email_field.send_keys('user1@example.com')
-        password_field.send_keys('1234')
-
-        # Send the form
-        password_field.send_keys(Keys.RETURN)
-
-        # Wait a little while to ensure that the action has been completed
-        time.sleep(4)
-
-        try:
-
-            driver.find_element(By.XPATH, "//h1[contains(@class, 'h2 mb-3') and contains(., 'Latest datasets')]")
-            print('Test passed!')
-
-        except NoSuchElementException:
-            raise AssertionError('Test failed!')
-
-    finally:
-
-        # Close the browser
-        close_driver(driver)
-
-
-# Call the test function
-test_login_and_check_element()
+        self.driver.get(f'{host}/login')
+        self.driver.find_element(By.ID, "email").send_keys("userCliente@gmail.com")
+        self.driver.find_element(By.ID, "password").send_keys("123456789!Aa")
+        self.driver.find_element(By.ID, "submit").click()
