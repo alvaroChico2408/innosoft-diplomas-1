d7c50405896972adee78e48ca2eb8b0b2e764356
Alejandro Aragon Sanchez <alejandro.as.2318@gmail.com>
2024-12-09 17:34:33 +0100

    [test]  login selenium test

--- app/modules/auth/tests/test_selenium.py
+++ app/modules/auth/tests/test_selenium.py
@@ -2,6 +2,9 @@ from selenium.webdriver.common.by import By
 
 from core.environment.host import get_host_for_selenium_testing
 from core.selenium.common import initialize_driver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import time
 
 class TestLogin():
@@ -16,6 +19,16 @@ class TestLogin():
         self.driver.get(get_host_for_selenium_testing())
         host = get_host_for_selenium_testing()
         self.driver.get(f'{host}/login')
-        self.driver.find_element(By.ID, "email").send_keys("userCliente@gmail.com")
-        self.driver.find_element(By.ID, "password").send_keys("12345678!Aa")
+        self.driver.find_element(By.ID, "email").send_keys("user1@example.com")
+        self.driver.find_element(By.ID, "password").send_keys("password")
         self.driver.find_element(By.ID, "submit").click()
+        try:
+            WebDriverWait(self.driver, 5).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, ".welcome-box h2.display-4.text-primary"))
+            )
+            
+            welcome_message = self.driver.find_element(By.CSS_SELECTOR, ".welcome-box h2.display-4.text-primary")
+            assert "Bienvenido a Diplomas INNOSOFT" in welcome_message.text
+
+        except Exception as e:
+            raise e
